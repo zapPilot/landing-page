e86756515f85be37e3af0b25abd51405e2ef6520
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2025-12-08 10:00:05 +0900

    wip: add do-nothing for neutral

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,3 @@
-export { Button } from './Button';
-export { Card } from './Card';
 export { TokenIcon, TokenPair } from './TokenIcon';
 export { SVGBox } from './svg/SVGBox';
 export { GlowFilter } from './svg/GlowFilter';
